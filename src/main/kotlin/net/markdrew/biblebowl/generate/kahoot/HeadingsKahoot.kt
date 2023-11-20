754e86d9cf416382526c8311b254d849dd8092c4
Mark Drew <mark@markdrew.net>
2023-11-19 19:21:24 -0600

    Generate full range of chapter headings kahoot files

--- src/main/kotlin/net/markdrew/biblebowl/generate/kahoot/HeadingsKahoot.kt
+++ src/main/kotlin/net/markdrew/biblebowl/generate/kahoot/HeadingsKahoot.kt
@@ -7,7 +7,6 @@ import net.markdrew.biblebowl.generate.practice.PracticeTest
 import net.markdrew.biblebowl.generate.practice.Round
 import net.markdrew.biblebowl.generate.practice.headingsCluePool
 import net.markdrew.biblebowl.model.BRIEF_BOOK_FORMAT
-import net.markdrew.biblebowl.model.Book.NUM
 import net.markdrew.biblebowl.model.ChapterRef
 import net.markdrew.biblebowl.model.PracticeContent
 import net.markdrew.biblebowl.model.StandardStudySet
@@ -19,31 +18,34 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 fun main() {
-    writeHeadingsKahoot(StandardStudySet.DEFAULT, throughChapter = NUM.chapterRef(1), randomSeed = 1)
+    val studySet: StudySet = StandardStudySet.DEFAULT
+    val studyData: StudyData = StudyData.readData(studySet, Paths.get(DATA_DIR))
+    // need to skip the first few chapters to ensure we're covering enough chapters for some possible wrong choices
+    for (throughChapter in studyData.chapterRefs.drop(5)) {
+        val content = studyData.practice(throughChapter)
+        writeHeadingsKahoot(content, randomSeed = 1)
+    }
 }
 
 private fun formatChapterRef(chapterRef: ChapterRef?): String =
     chapterRef?.format(BRIEF_BOOK_FORMAT) ?: "None of these"
 
 private fun writeHeadingsKahoot(
-    studySet: StudySet = StandardStudySet.DEFAULT,
-    throughChapter: ChapterRef? = null,
+    content: PracticeContent,
     numQuestions: Int = Int.MAX_VALUE,
     timeLimit: KahootTimeLimit = KahootTimeLimit.SEC_10,
     randomSeed: Int = Random.nextInt(1..9_999)
 ): File {
-    val setName = studySet.simpleName
-    val content: PracticeContent = StudyData.readData(studySet, Paths.get(DATA_DIR)).practice(throughChapter)
-
+    val setName = content.studyData.studySet.simpleName
     val practiceTest = PracticeTest(Round.EVENTS, content, numQuestions)
 
     val headingsToFind: List<MultiChoiceQuestion> = headingsCluePool(practiceTest, nChoices = 4)
 
     var fileName = "$setName-headings-kahoot"
     if (!content.allChapters) fileName += "-to-${content.coveredChapters.last().toString().lowercase()}"
-    fileName += "-%04d".format(randomSeed)
+//    fileName += "-%04d".format(randomSeed)
 
-    val xlsxFile = File("$PRODUCTS_DIR/$setName/kahoot/$fileName.xlsx")
+    val xlsxFile = File("$PRODUCTS_DIR/$setName/kahoot/headings/$fileName.xlsx")
     kahoot(xlsxFile) {
         for (multiChoice in headingsToFind) {
             question(
