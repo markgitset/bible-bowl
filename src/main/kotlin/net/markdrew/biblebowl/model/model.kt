bc56e91ad85a706452683149a78dfede16238ae7
Mark Drew <mark@markdrew.net>
2024-01-28 19:05:50 -0600

    Added some helpful comments

--- src/main/kotlin/net/markdrew/biblebowl/model/model.kt
+++ src/main/kotlin/net/markdrew/biblebowl/model/model.kt
@@ -1,7 +1,8 @@
 package net.markdrew.biblebowl.model
 
 /**
- * Used to combine books, chapters, and (optionally) verses into a single integer.  See [AbsoluteVerseNum]
+ * Used to combine books, chapters, and (optionally) verses into a single integer.  See [AbsoluteVerseNum].  Can also
+ * be thought of as one more than the maximum number of books, chapters (for a given book), or verses (for a given
+ * chapter)
  */
 const val BCV_FACTOR = 1_000
-
